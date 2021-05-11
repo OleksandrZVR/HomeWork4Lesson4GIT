c7a7ba476ef7ccd8f23397af7dc9dfe2e9606bfc
OleksandrZVR <qssxxx@ukr.net>
2021-05-11 22:55:14 +0300

    Add Home Work Lesson 13

---
+++ Lesson13AnimationCSSBeginningJSHomeWorkZVR/js/script.js
@@ -0,0 +1,35 @@
+let firstNumber = prompt('Введите первое число');
+console.log(firstNumber);
+
+let secondNumber = prompt('Введите второе число');
+console.log(secondNumber);
+
+let result = firstNumber * secondNumber;
+
+alert(result);
+console.log(result);
+
+
+let name = prompt('Введите имя');
+let lastName = prompt('Введите фамилию');
+let show = prompt('Название выступления');
+let theater = prompt('Название театра');
+let sector = prompt('Номер сектора');
+let row = prompt('Номер ряда');
+let place = prompt('Номер места');
+let time = prompt('Время выступления');
+
+let res = 'Имя: ' + name + '\n';
+res += 'Фамилия: ' + lastName + '\n';
+res += 'Выступление: ' + show + '\n';
+res += 'Театр: ' + theater + '\n';
+res += 'Сектор: ' + sector + '\n';
+res += 'Ряд: ' + row + '\n';
+res += 'Место: ' + place + '\n';
+res += 'Время: ' + time;
+
+console.log(res)
+
+
+
+
